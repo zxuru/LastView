73ee80d93156097bb9fcdd9fb2e7de2878ceaeed
zxuru <cualquiercosa833@gmail.com>
2021-11-14 03:12:37 -0300

    Update

--- app/src/main/java/com/lastview/Home_ACT.java
+++ app/src/main/java/com/lastview/Home_ACT.java
@@ -2,9 +2,14 @@ package com.lastview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Home_ACT extends AppCompatActivity {
+
+    private VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,16 @@ public class Home_ACT extends AppCompatActivity {
         if (extras != null) {
             String name = extras.getString("name");
         }
+
+        video = findViewById(R.id.videoView);
+
+        String ruta = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(ruta);
+        video.setVideoURI(uri);
+
+        video.start();
+
+        //MediaController media = new MediaController(this);
+        //video.setMediaController(media);
     }
 }
