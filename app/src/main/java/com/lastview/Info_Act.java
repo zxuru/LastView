b23184b0d0fc30295220a93631922bc37355a0e2
zxuru <cualquiercosa833@gmail.com>
2021-11-14 04:48:12 -0300

    Update

--- app/src/main/java/com/lastview/Info_Act.java
+++ app/src/main/java/com/lastview/Info_Act.java
@@ -2,6 +2,8 @@ package com.lastview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class Info_Act extends AppCompatActivity {
@@ -10,5 +12,12 @@ public class Info_Act extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        getSupportActionBar().hide();
+    }
+
+    public void  Marcado(){
+        Intent i = new Intent(Intent.ACTION_DIAL);
+        i.setData(Uri.parse("tel:"+"912345678"));
+        startActivity(i);
     }
 }
